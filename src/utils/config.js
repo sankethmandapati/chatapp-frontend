f335cd94dec0cd4707edb0c1fcb6a14725360334
sanketh.mandapti.contractor <sanketh.mandapti.contractor@bbva.com>
2019-04-25 22:15:36 +0530

    change environment ulr

--- src/utils/config.js
+++ src/utils/config.js
@@ -2,7 +2,7 @@ const urls = {
     dev: 'http://192.168.1.8:3005',
     prod: 'http://18.225.32.223:3005'
 };
-const env = "dev";
+const env = "prod";
 module.exports = {
     baseUrl: urls[env]
 };
